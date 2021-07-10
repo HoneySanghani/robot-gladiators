be5c0a769a194b20d0cb183639d2c3294473f060
Honey Sanghani <honeysanghani98@gmail.com>
2021-07-10 02:07:22 -0400

    add enemy combatants

--- assests/js/game.js
+++ assests/js/game.js
@@ -4,11 +4,11 @@ var playerHealth=100;
 var playerAttack=10;
 var playerMoney=10;
 //computer details
-var enemyName="Roborto";
+var enemyNames=["Roborto","Amy Android","Robo Trumble"];
 var enemyHealth=50;
 var enemyAttack=12;
 
-var fight=function(){
+var fight=function(enemyName){
     window.alert("Welcome to Robot Gladiators!");
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
     //option to fight or not 
@@ -49,4 +49,6 @@ var fight=function(){
         window.alert("You need to choose a valid option. Try again!");
     }
 }
-fight();
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+  }
